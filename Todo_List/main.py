cc482e653f2b75580a452d5ba731b169dfbd66d3
Aranya Majumdar <74165561+aranya-code@users.noreply.github.com>
2026-04-15 17:33:09 +0530

    add index page

--- Todo_List/main.py
+++ Todo_List/main.py
@@ -16,7 +16,7 @@ app.include_router(todos.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
